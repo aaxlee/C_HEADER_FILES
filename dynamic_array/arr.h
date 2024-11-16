c3b3a156125441106c3d467cdfde36ceeb367db4
aaxlee <143385379+aaxlee@users.noreply.github.com>
2024-11-16 18:26:41 +0100

    Add files via upload

---
+++ dynamic_array/arr.h
@@ -0,0 +1,47 @@
+#ifndef ARR_H
+#define ARR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_INIT(array_type, element_type)                                                           \
+array_type array_create_##element_type()                                                               \
+{                                                                                                      \
+  array_type a;                                                                                        \
+  a.capacity = 1;                                                                                      \
+  a.len = 0;                                                                                           \
+  a.data = (element_type*)malloc(a.capacity * sizeof(element_type));                                   \
+  return a;                                                                                            \
+}                                                                                                      \
+
+#define ARRAY_ADD(array_type, element_type)                                                            \
+void array_add_##element_type(array_type *array, element_type element)                                 \
+{                                                                                                      \
+  if (array->len >= array->capacity) {                                                                 \
+    array->capacity *= 2;                                                                              \
+    array->data = (element_type*)realloc(array->data, array->capacity * sizeof(element_type));         \
+  }                                                                                                    \
+  array->data[array->len] = (element);                                                                 \
+  array->len++;                                                                                        \
+}                                                                                                      \
+
+typedef struct
+{
+  int *data;
+  size_t len;
+  size_t capacity;
+} Array_Int;
+
+typedef struct
+{
+  char *data;
+  size_t len;
+  size_t capacity;
+} Array_Char;
+
+ARRAY_INIT(Array_Int, int)
+ARRAY_INIT(Array_Char, char)
+ARRAY_ADD(Array_Int, int)
+ARRAY_ADD(Array_Char, char)
+
+#endif /* ARR_H */
---
+++ dynamic_array/example.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "arr.h"
+
+int main()
+{
+  Array_Int array = array_create_int();
+  for (int i = 1; i < 11; i++) {
+    array_add_int(&array, i);
+  }
+
+  for (int i = 0; i < array.len; i++) {
+    printf("%d ", array.data[i]);
+  }
+
+  printf("Capacity: %d Length: %d\n", array.capacity, array.len);
+
+  return 0;
+}
