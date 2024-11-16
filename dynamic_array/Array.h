c10b7db9a42dda4896fba95ae9c12e5eadc3ae7e
aaxlee <143385379+aaxlee@users.noreply.github.com>
2024-11-16 20:16:50 +0100

    Update Array.h

--- dynamic_array/Array.h
+++ dynamic_array/Array.h
@@ -4,26 +4,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ARRAY_INIT(array_type, element_type)                                                           \
-array_type create_array_##element_type()                                                               \
-{                                                                                                      \
-  array_type a;                                                                                        \
-  a.capacity = 1;                                                                                      \
-  a.len = 0;                                                                                           \
-  a.data = (element_type*)malloc(a.capacity * sizeof(element_type));                                   \
-  return a;                                                                                            \
-}                                                                                                      \
+#define ARRAY_FUNCTIONS(array_type, element_type)                                                        \
+  array_type create_array_##element_type()                                                               \
+  {                                                                                                      \
+    array_type a;                                                                                        \
+    a.capacity = 1;                                                                                      \
+    a.len = 0;                                                                                           \
+    a.data = (element_type*)malloc(a.capacity * sizeof(element_type));                                   \
+    return a;                                                                                            \
+  }                                                                                                      \
+  void array_add_##element_type(array_type *array, element_type element)                                 \
+  {                                                                                                      \
+    if (array->len >= array->capacity) {                                                                 \
+      array->capacity *= 2;                                                                              \
+      array->data = (element_type*)realloc(array->data, array->capacity * sizeof(element_type));         \
+    }                                                                                                    \
+    array->data[array->len] = (element);                                                                 \
+    array->len++;                                                                                        \
+  }                                                                                                      \
 
-#define ARRAY_ADD(array_type, element_type)                                                            \
-void array_add_##element_type(array_type *array, element_type element)                                 \
-{                                                                                                      \
-  if (array->len >= array->capacity) {                                                                 \
-    array->capacity *= 2;                                                                              \
-    array->data = (element_type*)realloc(array->data, array->capacity * sizeof(element_type));         \
-  }                                                                                                    \
-  array->data[array->len] = (element);                                                                 \
-  array->len++;                                                                                        \
-}                                                                                                      \
+
 
 typedef struct Array_Int
 {
@@ -32,6 +32,13 @@ typedef struct Array_Int
   size_t capacity;
 } Array_Int;
 
+typedef struct Array_Float
+{
+  float *data;
+  size_t len;
+  size_t capacity;
+} Array_Float;
+
 typedef struct Array_Char
 {
   char *data;
@@ -39,9 +46,8 @@ typedef struct Array_Char
   size_t capacity;
 } Array_Char;
 
-ARRAY_INIT(Array_Int, int)
-ARRAY_INIT(Array_Char, char)
-ARRAY_ADD(Array_Int, int)
-ARRAY_ADD(Array_Char, char)
+ARRAY_FUNCTIONS(Array_Int, int)
+ARRAY_FUNCTIONS(Array_Float, float)
+ARRAY_FUNCTIONS(Array_Char, char)
 
 #endif /* ARRAY_H */
